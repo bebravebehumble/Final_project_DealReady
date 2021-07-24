1f7918a031651e2524e106447dd95eadfa1867f6
KSARecord <qlfneef91@naver.com>
2021-07-24 20:41:04 +0900

    Final_project_remove

--- Final_project_DealReady/portfolio_project/src/main/java/kr/icia/service/MainListBoService.java
+++
@@ -1,9 +0,0 @@
-package kr.icia.service;
-
-import java.util.List;
-
-import kr.icia.domain.UserStoreBoardVO;
-
-public interface MainListBoService {
-	public List<UserStoreBoardVO> getMainList();	// 물품 목록
-}
